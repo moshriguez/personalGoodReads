453e567640f75e225e943bbbc0142123bc4707f4
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2023-11-01 20:30:32 -0700

    add root endpoint and bson to model

--- main.go
+++ main.go
@@ -11,33 +11,33 @@ import (
 )
 
 type Book struct {
-	Title      string `json:"title"`
-	Author     string `json:"author"`
-	Read       bool
-	ReadOn     time.Time
-	LostInMove bool
+	Title      string    `bson:"title" json:"title"`
+	Author     string    `bson:"author" json:"author"`
+	Read       bool      `bson:"read" json:"read"`
+	ReadOn     time.Time `bson:"read_on" json:"read_on"`
+	LostInMove bool      `bson:"lost_in_move" json:"lost_in_move"`
 }
 
 var library []Book
 
 func GetBooks(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet!")
+	fmt.Fprintln(w, "GetBooks: not implemented yet!")
 }
 
 func AddBook(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet!")
+	fmt.Fprintln(w, "AddBook: not implemented yet!")
 }
 
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet!")
+	fmt.Fprintln(w, "UpdateBook: not implemented yet!")
 }
 
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet!")
+	fmt.Fprintln(w, "DeleteBook: not implemented yet!")
 }
 
 func GetBook(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet!")
+	fmt.Fprintln(w, "GetBook: not implemented yet!")
 }
 
 func main() {
@@ -45,6 +45,9 @@ func main() {
 	router := mux.NewRouter()
 
 	// endpoints
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to my library.")
+	}).Methods("GET")
 	router.HandleFunc("/books", GetBooks).Methods("GET")
 	router.HandleFunc("/books", AddBook).Methods("POST")
 	router.HandleFunc("/books/{id}", UpdateBook).Methods("PUT")
